445b487a6f34d0dd56e17fc329f68d3ceef17001
Josafat <josafatgarciasarmientos@gmail.com>
2024-09-20 17:57:54 -0600

    get devolution endpoint

--- api.py
+++ api.py
@@ -14,25 +14,64 @@ def get_employees():
 
 @app.route('/devolution', methods=['POST'])
 def post_devolution():
-    main_reason = "Example"
-    subsidiary = "Example"
-    explanation = "Example"
-    ticket_number = 1
-    client_number = 1
-    order_number = 1
-    date_product_arrived = "2020-05-22 09:06:28.580"
+    
+    data = request.get_json()
+    
+    main_reason = data.get('main_reason', "")
+    subsidiary = data.get('subsidiary', "")
+    explanation = data.get('explanation', "")
+    ticket_number = data.get('ticket_number', 0)
+    client_number = data.get('client_number', 0)
+    order_number = data.get('order_number', 0)
+    date_product_arrived = data.get('date_product_arrived', "")
+    items = data.get('items', [])
+    
+    
     db.insert_devolution(main_reason, subsidiary, explanation, ticket_number, client_number, order_number, date_product_arrived)
     details = db.get_devolutions()
-    print(details)
-    for detail in details:
-        var = detail
-    return var
-
-@app.route('/devolution', methods=['GET'])
-def get_devolution():
     
-    details = db.get_devolution(1)
-    return details
+    for item in items:
+        sku = item.get('sku')
+        quantity = item.get('cantidad', 1)  # Valor por defecto: 1
+        db.insert_devolution_item(details[-1][0], sku, quantity)
+        
+        
+    # Build Json with all data
+    
+    return jsonify(details[-1])
+
+@app.route('/devolution/<int:id>', methods=['GET'])
+def get_devolution(id):
+    devolution = db.get_devolution(id)
+    devolution_items = db.get_devolution_items(id)
+    
+    data = {
+        "id" : devolution[0][0],
+        "status": devolution[0][1],
+        "main_reason": devolution[0][2],
+        "subsidiary": devolution[0][3],
+        "explanation": devolution[0][4],
+        "ticket_number": devolution[0][5],
+        "client_number": devolution[0][6],
+        "order_number": devolution[0][7],
+        "createdAt": devolution[0][8],
+        "returnment_label": devolution[0][9],
+        "date_product_arrvie": devolution[0][10],
+        "shipping_payment": devolution[0][11],
+        "requires_label": devolution[0][12],
+        "items": []
+    }
+    
+    for item in devolution_items:
+        item_data = {
+            "sku": item[0],
+            "cantidad": item[1]
+        }
+        data["items"].append(item_data)
+    
+    return jsonify(data)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
--- rds.py
+++ rds.py
@@ -11,12 +11,23 @@ conn = pymysql.connect(
 
 def insert_devolution(main_reason, subsidiary, explanation, ticket_number, client_number, order_number, date_product_arrived):
     cur=conn.cursor()
-    cur.execute("INSERT INTO Devolution (mainReason,sucursal,explanation,ticketNumber,clientNumber,orderNumber,dateProductArrive) VALUES (%s,%s,%s,%s,%s,%s,%s)", (main_reason,subsidiary,explanation,ticket_number,client_number,order_number, date_product_arrived))
+    cur.execute("INSERT INTO Devolution (mainReason,sucursal,explanation,ticketNumber,clientNumber,orderNumber,dateProductArrive) VALUES (%s,%s,%s,%s,%s,%s,%s);", (main_reason,subsidiary,explanation,ticket_number,client_number,order_number, date_product_arrived))
+    conn.commit()
+
+def insert_devolution_item(devoulution_id, sku, qty):
+    cur = conn.cursor()
+    cur.execute("INSERT INTO Devolution_Item (devolutionId,sku,quantity) VALUES(%s,%s,%s);", (devoulution_id, sku, qty))
     conn.commit()
 
 def get_devolution(id):
     cur=conn.cursor()
-    cur.execute(f"SELECT * FROM Devolution WHERE id=${id}")
+    cur.execute("SELECT * FROM Devolution WHERE id= %s", (id))
+    details = cur.fetchall()
+    return details
+
+def get_devolution_items(id):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Devolution_Item WHERE devolutionId = %s;", (id))
     details = cur.fetchall()
     return details
 
